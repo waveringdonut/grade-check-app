eda9dfa09a75bab738711136bab186f782097357
waveringdonut <98999870+waveringdonut@users.noreply.github.com>
2023-04-20 11:58:26 +0900

    [clean] 科目区分を取得する方法を整理

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from PyPDF2 import PaperSize, PdfReader
+import configparser
+#config_ini.get('MYSQL', 'pass')
+config_ini = configparser.ConfigParser()
+config_ini.read('config.ini', encoding='utf-8')
 
 print("-------------------------------------------------------------")
 
@@ -7,35 +11,19 @@ with open("aa.pdf", "rb") as input:
     page = reader.pages[0] 
 
     # 読み込んだページのテキストを抽出
-    #print(page.extract_text())
+    #print(page.extract_text()) #pdfの文字全てを
 
     paper = page.extract_text()
     
     start = paper.index('≪') + 1  # 最初の'['の位置を特定し、次の文字の位置を取得
     p_2 = "≪"+paper[start:]
-    #print(p_2)
+    #print(p_2) #≪から先
     count_lines = p_2.count('\n') + 1
-    #print(count_lines)
+    #print(count_lines) #行数
 
     allSub_list = p_2.splitlines()
-    #print(allSub_list)
-    #print(allSub_list[1])
-
-    title_list = ['≪専門必修科目 (必修科目 )≫', '≪専門選択科目 (選択)≫', '≪基礎教育科目 (他学部)≫',
-                  '≪基礎教育科目 (実践・教養・心 )≫', '≪導入科目 (導入科目 )≫', '≪英語(英語)≫', '≪外国語科目 (外国語)≫']
-    index_list = [0]
-    i_1 = allSub_list.index(title_list[0])
-    index_list.append(allSub_list.index(title_list[1]))
-    index_list.append(allSub_list.index(title_list[2]))
-    index_list.append(allSub_list.index(title_list[3]))
-    index_list.append(allSub_list.index(title_list[4]))
-    index_list.append(allSub_list.index(title_list[5])) 
-    index_list.append(allSub_list.index(title_list[6]))            
-    #print(index_list)
-
-    '''search_value_2 = "アルゴリズム論"
-    doubleSub_index = [i for i, x in enumerate(allSub_list) if search_value_2 in x]
-    #print(doubleSub_index)'''
+    #print(allSub_list) #全ての行を1行毎、配列の要素として格納した配列
+    #print(allSub_list[1]) 
 
     #print(allSub_list[43])
     tmp = allSub_list[43].removeprefix("  ")
@@ -53,9 +41,17 @@ with open("aa.pdf", "rb") as input:
     allSub_list[44:44] = [back_sub]
     #print(len(allSub_list))
     #print(allSub_list)
-    search_value = "≪"
-    title_index_list = [i for i, x in enumerate(allSub_list) if search_value in x]
-    #print(title_index_list)
+
+    # 'a'を含む要素のインデックスを取得する
+    title_indices = [i for i, s in enumerate(allSub_list) if '≪' in s]
+    print(title_indices)
+
+    title_list = []
+    
+    for i in title_indices:
+        title_list.append(allSub_list[i])
+
+    print(title_list)
 
     hissyuu_list = []
     sentaku_list = []
@@ -68,10 +64,10 @@ with open("aa.pdf", "rb") as input:
     def data_list_cre(title, sub_list):
         if title in allSub_list:
             title_index = allSub_list.index(title)
-            hi_i = title_index_list.index(title_index)
+            hi_i = title_indices.index(title_index)
             next_index = 0
-            if hi_i+1 != len(title_index_list):
-                next_index = title_index_list[hi_i + 1]
+            if hi_i+1 != len(title_indices):
+                next_index = title_indices[hi_i + 1]
             else :
                 next_index = len(allSub_list)
             for i in range(title_index+1,next_index):
@@ -90,8 +86,8 @@ with open("aa.pdf", "rb") as input:
                 else :
                     subInfo_list_2.append('') 
                 sub_list.append(subInfo_list_2)
-                print(subInfo_list_2)
-        print('')
+                #print(subInfo_list_2)
+        #print('')
     
     data_list_cre(title_list[0], hissyuu_list)
     data_list_cre(title_list[1], sentaku_list)
@@ -101,7 +97,7 @@ with open("aa.pdf", "rb") as input:
     data_list_cre(title_list[5], eigo_list)
     data_list_cre(title_list[6], gaikokugo_list)
     
-    tanni_sum = 0
+    gpa_cal_tanni_sum = 0
     gp_sum = 0
 
     gp_dic = {'E':0, 'D':0, 'C':1, 'B':2, 'A':3, 'S':4}
@@ -114,13 +110,13 @@ with open("aa.pdf", "rb") as input:
         return tanni_sum
         
 
-    tanni_sum = add_tanni(hissyuu_list, tanni_sum)
-    tanni_sum = add_tanni(sentaku_list, tanni_sum)
-    tanni_sum = add_tanni(kisokyou_tagakubu_list, tanni_sum)
-    tanni_sum = add_tanni(kisokyou_list, tanni_sum)
-    tanni_sum = add_tanni(dounyuu_list, tanni_sum)
-    tanni_sum = add_tanni(eigo_list, tanni_sum)
-    tanni_sum = add_tanni(gaikokugo_list, tanni_sum)
+    gpa_cal_tanni_sum = add_tanni(hissyuu_list, gpa_cal_tanni_sum)
+    gpa_cal_tanni_sum = add_tanni(sentaku_list, gpa_cal_tanni_sum)
+    gpa_cal_tanni_sum = add_tanni(kisokyou_tagakubu_list, gpa_cal_tanni_sum)
+    gpa_cal_tanni_sum = add_tanni(kisokyou_list, gpa_cal_tanni_sum)
+    gpa_cal_tanni_sum = add_tanni(dounyuu_list, gpa_cal_tanni_sum)
+    gpa_cal_tanni_sum = add_tanni(eigo_list, gpa_cal_tanni_sum)
+    tangpa_cal_tanni_sumni_sum = add_tanni(gaikokugo_list, gpa_cal_tanni_sum)
 
     def add_gp(sub_list, gp_sum):
         if sub_list != []: #リストに何か追加されているとき
@@ -137,7 +133,7 @@ with open("aa.pdf", "rb") as input:
     gp_sum = add_gp(eigo_list, gp_sum)
     gp_sum = add_gp(gaikokugo_list, gp_sum)
     
-    gpa = gp_sum / tanni_sum
-    print(gpa) 
+    gpa = gp_sum / gpa_cal_tanni_sum
+    #print(gpa) 
 
 
